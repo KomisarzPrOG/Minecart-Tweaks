f342d031fd5a08ba3e77ec590beac80684e10a59
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2025-12-13 19:18:58 +0100

    Initial setup

---
+++ src/main/java/net/komisarzprog/minecart_tweaks/MinecartTweaksClient.java
@@ -0,0 +1,11 @@
+package net.komisarzprog.minecart_tweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecartTweaksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
