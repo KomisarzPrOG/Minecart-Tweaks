cd7c86d32e36508f10ce1cea832a9ef86e6a15fc
KomisarzPrOG <166999077+KomisarzPrOG@users.noreply.github.com>
2025-12-27 16:27:28 +0100

    Make lit furnace minecarts able to load chunks

--- src/main/java/net/komisarzprog/minecart_tweaks/mixin/FurnaceMinecartEntityMixin.java
+++ src/main/java/net/komisarzprog/minecart_tweaks/mixin/FurnaceMinecartEntityMixin.java
@@ -1,15 +1,17 @@
 package net.komisarzprog.minecart_tweaks.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.entity.vehicle.FurnaceMinecartEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ChunkTicketType;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,19 +19,36 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FurnaceMinecartEntity.class)
 public abstract class FurnaceMinecartEntityMixin {
-    @Shadow public abstract boolean isLit();
     @Shadow private int fuel;
+    @Shadow public abstract boolean isLit();
 
-    // Furnace Minecarts get faster
-    @Inject(method = "tick", at = @At("TAIL"))
-    private void minecarttweaks$boostSpeed(CallbackInfo ci)
+    @Unique ChunkPos previousChunk;
+
+    // Furnace Minecarts load chunks on their way
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void minecarttweaks$loadChunks(CallbackInfo info)
     {
-        FurnaceMinecartEntity self = (FurnaceMinecartEntity)(Object)this;
+        Entity self = (Entity)(Object)this;
+        World world = self.getEntityWorld();
 
-        if(self.isAlive() && fuel > 0)
+        if(world instanceof ServerWorld server)
         {
-            Vec3d velocity = self.getVelocity();
-            self.setVelocity(velocity.multiply(1.05));
+            ChunkPos currentChunk = ChunkSectionPos.from(self).toChunkPos();
+
+            if(previousChunk == null)
+                previousChunk = currentChunk;
+
+            if(fuel > 0)
+            {
+                server.getChunkManager().addTicket(ChunkTicketType.FORCED, currentChunk, 3);
+//                System.out.println("[MinecartTweaks] ADD ticket @ " + currentChunk);
+            }
+            if(!currentChunk.equals(previousChunk) || fuel <= 0)
+            {
+                server.getChunkManager().removeTicket(ChunkTicketType.FORCED, previousChunk, 3);
+//                System.out.println("[MinecartTweaks] REMOVE ticket @ " + previousChunk);
+            }
+            previousChunk = currentChunk;
         }
     }
 
@@ -37,9 +56,13 @@ public abstract class FurnaceMinecartEntityMixin {
     @Inject(method = "getMaxSpeed", at = @At("RETURN"), cancellable = true)
     private void minecarttweaks$boostMaxSpeed(CallbackInfoReturnable<Double> cir)
     {
-        if(isLit())
+        Vec3d velocity = ((FurnaceMinecartEntity)(Object)this).getVelocity();
+        boolean isTurning = Math.abs(velocity.x) != 0 && Math.abs(velocity.z) != 0;
+        double stableSpeed = 1.8D;
+
+        if(isLit() && !isTurning)
             // Boost minecart max speed to 2x the amount for now
-            cir.setReturnValue(0.8D);
+            cir.setReturnValue(stableSpeed);
         else
             cir.setReturnValue(0.4D);
     }
